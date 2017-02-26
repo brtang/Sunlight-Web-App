303c78fcf184724f86da0d4c9ed41a1e6dc0baa9
brtang <brtang@ucsc.edu>
2017-02-26 04:22:27 +0000

    Added create user query to users.js

--- db/repos/users.js
+++ db/repos/users.js
@@ -1,5 +1,8 @@
 module.exports = (rep, pgp) => {
    return{
+      add: values =>
+        rep.one('INSERT INTO Users(Name, User_Id, Company_Id, Role) VALUES ($1, $2, $3, $4)';,values, user => user.Name),
+        
       find: name =>
          rep.oneOrNone('SELECT * FROM Users WHERE Name = $1', name)
    };
--- index.js
+++ index.js
@@ -2,19 +2,22 @@
 //==========================================
 const express = require('express');
       pg = require('pg');
-      
+      db = require('./db');
+      config = require('./config/config');
+
+/*      
 var client = new pg.Client({
-  user: 'xxxxxxx',
-  password: 'xxxxxxx',
-  host: 'localhost',
-  database: 'xxxxxxx',
-  port: 5432, 
+  user: config.user,
+  password: config.password,
+  host: config.host,
+  database: config.database,
+  port: config.port, 
 });
 
 client.connect(function (err) {
   if (err) throw err;
   console.log("Connected to postgres");
-});
+});*/
 
 var app = express();
 app.set('port', process.env.PORT || 8080);
@@ -24,6 +27,62 @@ app.get('/', function(req,res){
   res.send("Hi! This is the great!");
 });
 
+//Find user by name
+app.get('/users/find/:name', (req,res) => {
+    db.users.find(req.params.name)
+        .then(data => {
+            console.log("This is data: ", data);
+            res.json({
+              success:true,
+              data
+            });  
+        })
+        .catch(error => {
+            console.log("Error: ", error);
+            res.json({
+                success: false,
+                error: error.message || error
+            });
+        });        
+});
+
+//Find group by name
+app.get('/groups/find/:name', (req,res) => {
+    db.groups.find(req.params.name)
+        .then(data => {
+            console.log("This is data: ", data);
+            res.json({
+              success:true,
+              data
+            });  
+        })
+        .catch(error => {
+            console.log("Error: ", error);
+            res.json({
+                success: false,
+                error: error.message || error
+            });
+        });        
+});
+
+//Find company by name
+app.get('/company/find/:name', (req,res) => {
+    db.users.find(req.params.name)
+        .then(data => {
+            console.log("This is data: ", data);
+            res.json({
+              success:true,
+              data
+            });  
+        })
+        .catch(error => {
+            console.log("Error: ", error);
+            res.json({
+                success: false,
+                error: error.message || error
+            });
+        });        
+});
 
 app.listen(app.get('port'), function(){
    console.log('Express started on http://localhost:' + 
