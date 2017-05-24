dd54fdceeffa465d2db679e8285b508081de3021
Brian Tang <brtang@ucsc.edu>
2017-05-23 20:36:40 -0700

    Added alert dialog, need to add links to other pages and get info icon

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 
-var app = angular.module('myApp', ['countTo', 'rzModule', 'ui.bootstrap','ngMaterial','mdDataTable']);
+var app = angular.module('myApp', ['countTo', 'rzModule', 'ui.bootstrap','ngMaterial','mdDataTable', 'ngMessages', 'material.svgAssetsCache']);
 
 
 app.factory("flash", function($rootScope) {
@@ -133,7 +133,7 @@ app.service('poleService', function($http, $httpParamSerializer) {
 
 });
 
-app.controller('MainController', ['$scope', '$http', '$httpParamSerializer', 'flash', 'userService', 'poleService', function($scope, $http, $httpParamSerializer, flash, userService, poleService){
+app.controller('MainController', ['$scope', '$http', '$httpParamSerializer', 'flash', 'userService', 'poleService', '$mdDialog', function($scope, $http, $httpParamSerializer, flash, userService, poleService, $mdDialog){
     console.log("Made it to main controller");
     $scope.flashService = flash;
     $scope.poleList = []; 
@@ -152,6 +152,23 @@ app.controller('MainController', ['$scope', '$http', '$httpParamSerializer', 'fl
         }
     };
     */
+    
+    $scope.showAlert = function(ev) {
+    // Appending dialog to document.body to cover sidenav in docs app
+    // Modal dialogs should fully cover application
+    // to prevent interaction outside of dialog
+    $mdDialog.show(
+      $mdDialog.alert()
+        .parent(angular.element(document.querySelector('#popupContainer')))
+        .clickOutsideToClose(true)
+        .title('This is an alert title')
+        .textContent('You can specify some description text in here.')
+        .ariaLabel('Alert Dialog Demo')
+        .ok('Got it!')
+        .targetEvent(ev)
+    );
+  };
+    
     
     var profile = userService.fetchUserdata();     
     profile.then(function(result){
