932bb3c7fc0085255d22a40a405f96e4bd5c29be
Brian Tang <brtang@ucsc.edu>
2017-09-20 21:07:59 -0700

    Masked out config for github

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 var config = {
-   user: 'sunlight',
-   password: 'amazonwebservices',
-   host: 'aa1tcwr0sfvotku.chwdxjjrbb7a.us-west-2.rds.amazonaws.com',
-   database: 'ebdb',
-   port: 5432,
-   secret: 'supersecretrandompassword'
+   user: 'xxxxxx',
+   password: 'xxxxxx',
+   host: 'xxxxxx',
+   database: 'xxxxxx',
+   port: xxxxxx,
+   secret: 'xxxxxx'
 };
 
 module.exports = config;
